d8fd537365df447e88e751632263b557c83e4cd3
Sombra0101 <147946325+Sombra0101@users.noreply.github.com>
2023-10-14 18:08:15 -0600

    Baner script

---
+++ banner.py
@@ -0,0 +1,28 @@
+
+print("""                                                                                                                                       
+                                                                                                                                      
+HHHHHHHHH     HHHHHHHHH               AAA                  CCCCCCCCCCCCCKKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   
+H:::::::H     H:::::::H              A:::A              CCC::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ER::::::::::::::::R  
+H:::::::H     H:::::::H             A:::::A           CC:::::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ER::::::RRRRRR:::::R 
+HH::::::H     H::::::HH            A:::::::A         C:::::CCCCCCCC::::CK:::::::K   K::::::KEE::::::EEEEEEEEE::::ERR:::::R     R:::::R
+  H:::::H     H:::::H             A:::::::::A       C:::::C       CCCCCCKK::::::K  K:::::KKK  E:::::E       EEEEEE  R::::R     R:::::R
+  H:::::H     H:::::H            A:::::A:::::A     C:::::C                K:::::K K:::::K     E:::::E               R::::R     R:::::R
+  H::::::HHHHH::::::H           A:::::A A:::::A    C:::::C                K::::::K:::::K      E::::::EEEEEEEEEE     R::::RRRRRR:::::R 
+  H:::::::::::::::::H          A:::::A   A:::::A   C:::::C                K:::::::::::K       E:::::::::::::::E     R:::::::::::::RR  
+  H:::::::::::::::::H         A:::::A     A:::::A  C:::::C                K:::::::::::K       E:::::::::::::::E     R::::RRRRRR:::::R 
+  H::::::HHHHH::::::H        A:::::AAAAAAAAA:::::A C:::::C                K::::::K:::::K      E::::::EEEEEEEEEE     R::::R     R:::::R
+  H:::::H     H:::::H       A:::::::::::::::::::::AC:::::C                K:::::K K:::::K     E:::::E               R::::R     R:::::R
+  H:::::H     H:::::H      A:::::AAAAAAAAAAAAA:::::AC:::::C       CCCCCCKK::::::K  K:::::KKK  E:::::E       EEEEEE  R::::R     R:::::R
+HH::::::H     H::::::HH   A:::::A             A:::::AC:::::CCCCCCCC::::CK:::::::K   K::::::KEE::::::EEEEEEEE:::::ERR:::::R     R:::::R
+H:::::::H     H:::::::H  A:::::A               A:::::ACC:::::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ER::::::R     R:::::R
+H:::::::H     H:::::::H A:::::A                 A:::::A CCC::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ER::::::R     R:::::R
+HHHHHHHHH     HHHHHHHHHAAAAAAA                   AAAAAAA   CCCCCCCCCCCCCKKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRR
+                                                                                                                                      
+                                                                                                                                      
+                                                                                                                                      
+                                                                                                                                      
+                                                                                                                                      
+                                                                                                                      
+                                                                                                                                      
+                                          """)
+                                                 
